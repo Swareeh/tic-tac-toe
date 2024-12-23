f3c7e32c769ed5238b6f74463227202d6f6fac26
Swareeh <77015406+Swareeh@users.noreply.github.com>
2024-12-24 00:20:53 +0400

    Referee Simplified

--- script.py
+++ script.py
@@ -13,7 +13,10 @@ def Player1Input():
     column = input('Enter column no: ')
     
     try:
-        board[int(row)-1][int(column)-1] = 'X'
+        if board[int(row)-1][int(column)-1] == 'X' or board[int(row)-1][int(column)-1] == 'O':
+            print('Postion Already filled!')
+        else:
+            board[int(row)-1][int(column)-1] = 'X'
     except:
         print('Invalid input!')
     
@@ -22,54 +25,44 @@ def Player2Input():
     column = input('Enter column no: ')
     
     try:
-        board[int(row)-1][int(column)-1] = 'O'
+        if board[int(row)-1][int(column)-1] == 'X' or board[int(row)-1][int(column)-1] == 'O':
+            print('Postion Already filled!')
+        else:
+            board[int(row)-1][int(column)-1] = 'O'
     except:
         print('Invalid input!')
-    
-#FIXME: Referee does not know the rules of the game lol
+
 def Referee():
     global gameOver
-    #Checking if Player X won
-    if board[0][0] == board[0][1] == board[0][2] == 'X':
-        print('Player X Wins')
-        gameOver = True
-        
-    elif board[1][0] == board[1][1] == board[1][2] == 'X':
-        print('Player X Wins')
-        gameOver = True
-        
-    elif board[2][0] == board[2][1] == board[2][2] == 'X':
-        print('Player X Wins')
-        gameOver = True
-        
-    elif board[0][0] == board[1][1] == board[2][2] == 'X':
-        print('Player X Wins')
-        gameOver = True
-        
-    elif board[0][2] == board[1][1] == board[2][0] == 'X':
-        print('Player X Wins')
-        gameOver = True
 
-    #Checking if player O wins
-    elif board[0][0] == board[0][1] == board[0][2] == 'O':
-        print('Player O Wins')
-        gameOver = True
-        
-    elif board[1][0] == board[1][1] == board[1][2] == 'O':
-        print('Player O Wins')
-        gameOver = True
-        
-    elif board[2][0] == board[2][1] == board[2][2] == 'O':
-        print('Player O Wins')
-        gameOver = True
-        
-    elif board[0][0] == board[1][1] == board[2][2] == 'O':
-        print('Player O Wins')
-        gameOver = True
-        
-    elif board[0][2] == board[1][1] == board[2][0] == 'O':
-        print('Player O Wins')
-        gameOver = True
+    for i in range(3):
+        for j in ['X','O']:
+            if board[i][0] == board[i][1] == board[i][2] == j:
+                print('Player',j,'Wins!')
+                gameOver = True
+                
+            elif board[0][i] == board[1][i] == board[2][i] == j:
+                print('Player',j,'Wins!')
+                gameOver = True
+
+            elif board[0][0] == board[1][1] == board[2][2] == j:
+                print('Player',j,'Wins!')
+                gameOver = True
+
+            elif board[0][2] == board[1][1] == board[2][0] == j:
+                print('Player',j,'Wins!')
+                gameOver = True
+
+
+#FIXME: Decision to make it a draw
+    # empty_space = True
+    # for i in board:
+    #     for j in i:
+    #         if j == '_':
+    #             break
+
+
+
           
 #FIXME: the game flow is a disaster
 while gameOver==False:
@@ -81,14 +74,18 @@ while gameOver==False:
     print()
     Referee()
     if gameOver == True:
+        DisplayBoard()
         break
     DisplayBoard()
     print()
     print()
     Player2Input()
+    Referee()
     if gameOver == True:
+        DisplayBoard()
         break
 
 
 #TODO:
+#Don't skip the players turn if the player provides a invalid spot/a spot that is already filled
 #2 modes AI and Multiplayer Modes
